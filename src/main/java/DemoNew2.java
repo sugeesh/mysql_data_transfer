743b936e07806502ba6d945545044ad345e9452e
sugeesh <lpdneo@gmail.com>
2018-01-17 16:36:08 +0530

    Image url update added

--- src/main/java/DemoNew2.java
+++ src/main/java/DemoNew2.java
@@ -16,7 +16,7 @@ public class DemoNew2 {
         Connection connectionDatabase2 = null;
         try {
             connectionDatabase1 = DBConnection.getDBConnection("jdbc:mysql://localhost/president_test1?useUnicode=yes&characterEncoding=UTF-8", "root", "").getConnection();
-            connectionDatabase2 = DBConnection.getDBConnection("jdbc:mysql://localhost/president_new?useUnicode=yes&characterEncoding=UTF-8", "root", "").getConnection();
+            connectionDatabase2 = DBConnection.getDBConnection("jdbc:mysql://localhost/president_new_test?useUnicode=yes&characterEncoding=UTF-8", "root", "").getConnection();
 
 //            DBHandle.setData(connectionDatabase1,"INSERT INTO Persons(PersonID,LastName) VALUES('5','සිංහල පංතිය')");
         } catch (ClassNotFoundException e) {
@@ -28,7 +28,7 @@ public class DemoNew2 {
 
         ResultSet resultSet = null;
         try {
-            resultSet = DBHandle.getData(connectionDatabase1, "SELECT * FROM wp_posts limit 50");
+            resultSet = DBHandle.getData(connectionDatabase1, "SELECT * FROM wp_posts");
         } catch (SQLException e) {
             printErrorOutput("Error in getting data from database", e);
         }
@@ -38,28 +38,12 @@ public class DemoNew2 {
 
                 // Logic here
                 Integer id = resultSet.getInt("ID");
-                String post_title = resultSet.getString("post_title");
-                String post_content = resultSet.getString("post_content");
-                Date post_date = resultSet.getDate("post_date");
-                String post_excerpt = resultSet.getString("post_excerpt");
-                String post_status = resultSet.getString("post_status");
-                String comment_status = resultSet.getString("comment_status");
                 Integer post_parent = resultSet.getInt("post_parent");
                 String guid = resultSet.getString("guid");
                 String post_type = resultSet.getString("post_type");
 
-                DateFormat dateFormatReference = new SimpleDateFormat("yyyy-MMMM-dd");
-                String post_date_string = dateFormatReference.format(post_date)+"-news-gov-"+id;
-
                 if("attachment".equals(post_type)) {
-
-                    PreparedStatement stmt = connectionDatabase2.prepareStatement("UPDATE news SET featureImageUrl='"+guid+"' WHERE id='"+post_parent+"'");
-
-                    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    java.util.Date date = new java.util.Date();
-                    stmt.setString(12, dateFormat.format(date));
-                    stmt.setString(13, dateFormat.format(date));
-                    stmt.executeUpdate();
+                    DBHandle.setData(connectionDatabase2,"UPDATE news SET featureImageUrl='"+guid+"' WHERE id='"+post_parent+"'");
                 }
 
             }
